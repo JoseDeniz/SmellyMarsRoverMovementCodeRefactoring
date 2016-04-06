4b520c39f2650f1ad0f4ccf59c611ee85227641f
Alvaro Garcia <derecerca@gmail.com>
2016-04-06 10:12:53 +0300

    refactor: duplicate conditional so we can invert the order of the conditional axes

--- src/Rover.java
+++ src/Rover.java
@@ -42,6 +42,34 @@ public class Rover {
                         direction = "N";
                     }
                 }
+            } else if (command.equals("l") || command.equals("r")) {
+
+                // Rotate Rover
+                if (direction.equals("N")) {
+                    if (command.equals("r")) {
+                        direction = "E";
+                    } else {
+                        direction = "W";
+                    }
+                } else if (direction.equals("S")) {
+                    if (command.equals("r")) {
+                        direction = "W";
+                    } else {
+                        direction = "E";
+                    }
+                } else if (direction.equals("W")) {
+                    if (command.equals("r")) {
+                        direction = "N";
+                    } else {
+                        direction = "S";
+                    }
+                } else {
+                    if (command.equals("r")) {
+                        direction = "S";
+                    } else {
+                        direction = "N";
+                    }
+                }
             } else {
                 displaceRover(command);
             }
